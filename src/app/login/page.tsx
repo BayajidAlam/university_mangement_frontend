82cff22779db177121eb32461f6d2883896b0afe
BayajidAlam <bayajidalam2001@gmail.com>
2023-09-25 21:31:36 +0600

    reuseable breadcrumb created

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,65 @@
-import React from 'react';
+"use client";
 
+import { Button, Col, Row } from "antd";
+import loginImage from "../../assets/login.png";
+import Image from "next/image";
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import { SubmitHandler } from "react-hook-form";
+
+type FormValues = {
+  id: string;
+  password: string;
+};
 const LoginPage = () => {
+  const onSubmit: SubmitHandler<FormValues> = async (data) => {
+    try {
+      console.log(data);
+    } catch (error) {}
+  };
   return (
-    <div>
-      <h1>This is login page</h1>
-    </div>
+    <Row
+      justify="center"
+      align="middle"
+      style={{
+        minHeight: "100vh",
+      }}
+    >
+      <Col sm={12} md={16} lg={10}>
+        <Image src={loginImage} width={500} alt="login-image"></Image>
+      </Col>
+      <Col sm={12} md={8} lg={8}>
+        <h1
+          style={{
+            margin: "15px 0px",
+          }}
+        >
+          First Login your account
+        </h1>
+        <div>
+          <Form submitHandler={onSubmit}>
+            <div>
+              <FormInput name="id" type="text" size="large" label="User Id" />
+            </div>
+            <div
+              style={{
+                margin: "15px 0px",
+              }}
+            >
+              <FormInput
+                name="password"
+                type="password"
+                size="large"
+                label="User Password"
+              />
+            </div>
+            <Button type="primary" htmlType="submit">
+              Login
+            </Button>
+          </Form>
+        </div>
+      </Col>
+    </Row>
   );
 };
 
