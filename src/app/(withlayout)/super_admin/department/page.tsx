535503e82128189f7a2f824c75d5eca1c0714f2f
BayajidAlam <bayajidalam2001@gmail.com>
2023-10-04 12:46:27 +0600

    department created using rtk query

--- src/app/(withlayout)/super_admin/department/page.tsx
+++ src/app/(withlayout)/super_admin/department/page.tsx
@@ -1,9 +1,23 @@
+import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
+import { Button } from "antd";
+import Link from "next/link";
 import React from "react";
 
 const ManageDepartment = () => {
   return (
-    <div>
-      <h1>Manage Department</h1>
+    <div style={{ padding: "10px" }}>
+      <UMBreadCrumb
+        items={[
+          {
+            label: "super_admin",
+            link: "/super_admin",
+          },
+        ]}
+      />
+      <h1>Department List</h1>
+      <Link href="/super_admin/department/create">
+        <Button type="primary">Create</Button>
+      </Link>
     </div>
   );
 };
